dcabc85c1d28f917ebb6e049bd740aa82e2dbc0f
Josh De Winne <josh@dewinne.dev>
2023-01-09 19:34:52 -0800

    Using diagnostics for processing results

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,8 @@ import got, { PlainResponse } from 'got';
 import * as fs from 'fs';
 import path = require('path/posix');
 import { allowedNodeEnvironmentFlags } from 'node:process';
+import { languages, Diagnostic, DiagnosticSeverity } from 'vscode';
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -16,6 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "replicated" is now active!');
+	let diagnosticCollection = languages.createDiagnosticCollection("replicated");
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -24,12 +27,13 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		if (vscode.workspace.workspaceFolders? vscode.workspace.workspaceFolders.length > 0 : false) {
+			diagnosticCollection.clear();
 			(vscode.workspace.workspaceFolders? vscode.workspace.workspaceFolders : []).forEach(function (fldr) {
 				const absolutePath = fldr.uri.path;
 				const manifestFldr = vscode.workspace.getConfiguration('replicated').get("manifestsFolder");
 				
 				
-				getlintdata(path.join(absolutePath, String(manifestFldr)));
+				getlintdata(path.join(absolutePath, String(manifestFldr)), diagnosticCollection);
 					
 				
 			});
@@ -39,7 +43,7 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-function getlintdata(fldr: string) {
+function getlintdata(fldr: string, diagnosticCollection: vscode.DiagnosticCollection) {
 	const pipeline = promisify(stream.pipeline);
 
 	let readable = tar.c({}, [fldr]);
@@ -56,7 +60,39 @@ function getlintdata(fldr: string) {
 		let rawData = '';
 		response.on('data',  (chunk) => { rawData += chunk; });
 		response.on('end', () => {
-			vscode.window.showInformationMessage('Replicated Lint results: '  + rawData);
+			let lintResults = JSON.parse(rawData);
+			let map = new Map();
+
+
+			lintResults.lintExpressions.forEach(function (lintExpression: { path: string; }) {
+				if (map.has(lintExpression.path)) {
+					var exps = map.get(lintExpression.path);
+					exps.push(lintExpression);
+					map.set(lintExpression.path,exps);
+				} else {
+					map.set(lintExpression.path, [lintExpression]);
+				}
+			});
+
+			
+			for (let key of map.keys()) {
+				let diagnostics : Diagnostic[] = [];
+				for (let lexpr of map.get(key)) {
+					for (let pos of lexpr.positions) {
+						let range = new vscode.Range(new vscode.Position(pos.start.line-1,0),new vscode.Position(pos.start.line-1,0));
+						let message = "Replicated: " + lexpr.message;
+						let severity = DiagnosticSeverity.Warning;
+						if (lexpr.type === "info") {
+							severity = DiagnosticSeverity.Information;
+						}
+						if (lexpr.type === "error") {
+							severity = DiagnosticSeverity.Error;
+						}
+						diagnostics.push(new Diagnostic(range, message, severity));
+					}
+				}
+				diagnosticCollection.set(vscode.Uri.file(key), diagnostics);
+			}
 		});
 		
 	});
